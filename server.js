cbfdcba847d99f31ed3b8cb406735ae418f74a09
Alba Mendez <me@alba.sh>
2018-04-08 23:52:21 +0200

    Use Promises for safer code!

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const tmp = require("tmp")
+const util = require("util")
+const tmp = require("tmp-promise")
 const fs = require("fs")
-const { execFile, execFileSync } = require("child_process")
+const { execFileSync } = require("child_process")
 const botgram = require("botgram")
+
+const execFile = util.promisify(require("child_process").execFile)
+const fileStream = util.promisify(bot.fileStream.bind(bot))
+const pipe = (src, dest, options) => new Promise((resolve, reject) => {
+    src.on("error", reject)
+    dest.on("error", reject)
+    src.pipe(dest, options).on("finish", () => resolve())
+})
 
 try {
     config = require("./config.js")
@@ -25,39 +34,42 @@ bot.sticker((msg, reply) => {
     // send "uploading photo to the user"
     reply.action("upload_photo")
 
-    // start downloading sticker
-    bot.fileStream(msg.file, (err, stream) => {
-        if (err) {
-            console.error("Error when downloading sticker!\n%s", err.stack)
-            return reply.text("Oops! I couldn't download that sticker 😔")
-        }
-
-        stream.on("error", (err) => {
-            console.error("Error when getting sticker data!\n%s", err.stack)
-        })
-
-        // create temporal file, write sticker to it
-        tmp.file({ postfix: ".webp" }, (err, path, fd, cleanupCallback) => {
-            stream.pipe(fs.createWriteStream(null, { fd }))
-                .on("finish", () => stickerWritten(path, cleanupCallback))
-        })
-    })
-
-    function stickerWritten(path, cleanupCallback) {
-        // convert webp to png
-        execFile(config.dwebp, [path, "-o", "-"],
-            { encoding: "buffer", maxBuffer: 10 * 1024 * 1024 },
-            (err, stdout, stderr) => {
-            if (err) {
-                console.error("Conversion failed!", err, stderr.toString())
-                return reply.text("Sorry, conversion failed")
-            }
-            stdout.options = "sticker.png";
-            reply.document(stdout)
-        })
-    }
+    convertSticker(msg.file, reply).catch((err) => {
+        reply.text("Oops! I couldn't convert that sticker 😔")
+        console.error("Error when converting sticker %s:\n%s", msg.file.id, err.stack)
+    });
 })
 
 bot.command("start", "help", "usage", (msg, reply) => {
     reply.text("Hi! Send me stickers and I'll convert them to 🖼 .png images, keeping the transparency.")
 })
+
+// Base function to convert a sticker and upload the result to the user
+// Returns: Promise for the sent Message
+
+async function convertSticker(stickerFile, reply) {
+    // start sticker download
+    const streamPromise = fileStream(stickerFile)
+
+    // create temporal file
+    const tmpFilePromise = tmp.file({ postfix: ".webp" })
+
+    // wait until both are ready, and copy data from one into another
+    const [ stream, tmpFile ] = await Promise.all([ streamPromise, tmpFilePromise ])
+    const tmpStream = fs.createWriteStream(null, { fd: tmpFile.fd })
+    await pipe(stream, tmpStream)
+
+    // convert webp to png
+    const convertPromise = execFile(config.dwebp, [tmpFile.path, "-o", "-"],
+        { encoding: "buffer", maxBuffer: 10 * 1024 * 1024 }).catch((err) => {
+        throw Error("Conversion failed, code %s, signal %s, stderr %s",
+            err.code, err.signal, util.inspect(err.stderr))
+    })
+    const { stdout } = await convertPromise
+    tmpFile.cleanup()
+
+    // send as document
+    stdout.options = "sticker.png"
+    reply.document(stdout)
+    return await reply.then()
+}
